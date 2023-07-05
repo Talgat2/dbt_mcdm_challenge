3512a33b73e26f488e85b01e0f6906cdd175216a
Talgat2 <43268613+Talgat2@users.noreply.github.com>
2023-07-06 01:37:42 +0600

    Create bing.sql

---
+++ models/bing.sql
@@ -0,0 +1,10 @@
+select
+'Bing' as channel,
+date as `date`,
+sum(spend) as spend,
+0 as engagements,
+sum(conv) as total_conversions,
+sum(imps) as impressions,
+sum(clicks) as clicks
+from {{ ref('src_ads_bing_all_data')}}
+group by channel, `date`;
